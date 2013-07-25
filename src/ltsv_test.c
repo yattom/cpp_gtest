f11de65be2e60c3609b3f6d622b95f85233c2dda
yattom <tsutomu.yasui@gmail.com>
2013-07-25 17:09:42 +0900

    rafactoring tests to introduce ltsv_delete()

--- src/ltsv_test.c
+++ src/ltsv_test.c
@@ -1,21 +1,29 @@
 #include "ltsv.h"
 #include "gtest/gtest.h"
 
-static char buf[1000] = "uninitialized";
 
-const char* dump(LTSV ltsv)
-{
-	ltsv_dump(ltsv, buf, sizeof(buf)/sizeof(buf[0]));
-	return buf;
-}
+class LtsvTests : public ::testing::Test {
+public:
+	char buf[1000];
+	LTSV ltsv;
+	virtual void SetUp() {
+		strcpy(buf, "uninitialized");
+		ltsv = ltsv_init();
+	}
 
-TEST(LtsvTest, Empty) {
-	LTSV ltsv = ltsv_init();
+	const char* dump(LTSV ltsv)
+	{
+		ltsv_dump(ltsv, buf, sizeof(buf)/sizeof(buf[0]));
+		return buf;
+	}
+
+};
+
+TEST_F(LtsvTests, Empty) {
 	EXPECT_STREQ("", dump(ltsv));
 }
 
-TEST(LtsvTest, SetAndGet) {
-	LTSV ltsv = ltsv_init();
+TEST_F(LtsvTests, SetAndGet) {
 	ltsv_set(ltsv, "foo", "bar");
 	EXPECT_STREQ("bar", ltsv_get(ltsv, "foo"));
 	EXPECT_STREQ("foo:bar\n", dump(ltsv));
